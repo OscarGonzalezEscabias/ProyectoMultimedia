d83197e2c4820754cb4bd5f551eb04b09562c606
OSCR <oscarge10.oge@gmail.com>
2024-10-14 23:09:39 +0200

    Segundo commit

--- app/src/main/java/com/example/prueba1/view/Dialog.kt
+++ app/src/main/java/com/example/prueba1/view/Dialog.kt
@@ -1,8 +1,53 @@
 package com.example.prueba1.view
 
+import com.example.prueba1.data.RepositoryClient
 import com.example.prueba1.logic.Controller
+import com.example.prueba1.logic.interfaces.OperationsInterface
 
 class Dialog(var controller : Controller) {
+    private var listener: OperationsInterface? = null
+
+    private var action : Int = 0
 
 
+    fun setListener ( _listener : OperationsInterface){
+        listener = _listener
+
+    }
+
+
+    fun show(typeAction : Int){
+        listener?.let{
+            val posibleName = "CAMBIADO"
+            val posibleId = controller.devIdRandom()
+            when (typeAction){
+                0 -> onAccept()
+
+                1 ->
+                    if (posibleId != -1)
+                        onEdit(posibleId, "CAMBIADO")
+
+                2 ->
+                    if (posibleId != -1)
+                        onDelete(posibleId)
+
+            }
+
+        }
+    }
+
+    private fun onDelete(id : Int) {
+        listener!!.ClientDel(id)
+    }
+
+    private fun onEdit(id: Int, name : String) {
+        listener!!.ClientUpdate(id, name)
+    }
+
+
+    private fun onAccept() {
+        listener!!.ClientAdd(RepositoryClient.incrementPrimary(), "NUEVO CLIENTE")
+    }
 }
+
+
--- app/src/main/java/com/example/prueba1/view/MainActivity.kt
+++ app/src/main/java/com/example/prueba1/view/MainActivity.kt
@@ -8,58 +8,109 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.prueba1.R
+import com.example.prueba1.logic.Client
+import com.example.prueba1.logic.Controller
 import com.example.prueba1.logic.interfaces.OperationsInterface
 
 class MainActivity : AppCompatActivity(), OperationsInterface {
-    private lateinit var myButtonAdd : ImageView
-    private lateinit var myButtonUpdate : ImageView
-    private lateinit var myButtonDelete : ImageView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //    insets
-        //}
-
-        Log.d(TAG, "Esto es un ejemplo de log")
-        start();
-    }
-
-    private fun start() {
-        myButtonAdd = findViewById(R.id.iv_add)
-        myButtonUpdate = findViewById(R.id.iv_edit)
-        myButtonDelete = findViewById(R.id.iv_delete)
-
-        myButtonAdd.setOnClickListener{
-            Log.d(TAG, "Boton de añadir")
-        }
-
-        myButtonUpdate.setOnClickListener{
-            Log.d(TAG, "Boton de editar")
-        }
-
-        myButtonDelete.setOnClickListener{
-            Log.d(TAG, "Boton de borrar")
-        }
-    }
+    private lateinit var myButtonAdd: ImageView
+    private lateinit var myButtonUpdate: ImageView
+    private lateinit var myButtonDel: ImageView
+    private lateinit var myDialog : Dialog
+    private val controller= Controller()
 
     companion object {
         const val TAG = "---SALIDA---"
     }
 
-    override fun ClientAdd(id: Int, name: String) {
-        TODO("Not yet implemented")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //      enableEdgeToEdge()  //barra superior transparente. App de borde a borde (toaaaa)
+        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        Log. d(TAG, "Esto es un ejemplo de log")
+        start()
+    }
+
+
+
+
+
+    private fun start() {
+        myButtonAdd = findViewById(R.id.iv_add)
+        myButtonUpdate = findViewById(R.id.iv_edit)
+        myButtonDel = findViewById(R.id.iv_delete)
+        myDialog = Dialog(controller)
+
+        myDialog.setListener(this)
+
+        myButtonAdd.setOnClickListener{
+            myDialog.show(0)
+        }
+
+        myButtonUpdate.setOnClickListener{
+            myDialog.show(1)
+
+        }
+
+        myButtonDel.setOnClickListener( {
+            myDialog.show(2)
+
+        })
+
+
+    }
+
+
+    override fun ClientAdd(id: Int, name: String){
+        val newClient = Client (id, name)
+        controller.ClientAddController(newClient)
+        var msg =  "El cliente con id = $id, ha sido insertado correctamente"
+
+        Log.d(TAG, msg)
+        showConsoleData(msg)
     }
 
     override fun ClientDel(id: Int) {
-        TODO("Not yet implemented")
+        var msg = ""
+        val delete = controller.ClientDelController(id)  //borramos
+
+        if (delete)
+            msg =  "El cliente con id = $id, ha sido eliminado correctamente"
+        else
+            msg = "El cliente con id = $id, no ha sido encontrado para eliminar"
+
+        Log. d(TAG, msg)
+        showConsoleData(msg)
+
     }
 
+
+
     override fun ClientUpdate(id: Int, name: String) {
-        TODO("Not yet implemented")
+        var msg = ""
+        val update = controller.ClientEditController(id, name)
+
+        if (update)
+            msg =  "El cliente con id = $id, ha sido eliminado correctamente"
+        else
+            msg = "El cliente con id = $id, no ha sido encontrado para eliminar"
+
+        Log. d(TAG, msg)
+        showConsoleData(msg)
+
     }
+
+    fun showConsoleData(msg:String){
+        val msg = controller.showData()
+        Thread.sleep(2000)
+        Log. d(TAG, msg)
+    }
+
 }
